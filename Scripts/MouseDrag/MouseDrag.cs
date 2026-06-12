6c9161978c29e3407b807a570f1c54ca60f0aa21
Kamil Janowicz <57400342+JanowiczKa@users.noreply.github.com>
2026-06-12 22:19:12 +0100

    Temp assets, Bar scene, Glass scene and mouse drag scripts (#3)
    * Temp assets, Bar scene, Glass scene and mouse drag scripts
    
    * change main scene to BarScene
    
    * Re-organise file system
    
    * Moved BarScene

---
+++ Scripts/MouseDrag/MouseDrag.cs
@@ -0,0 +1,55 @@
+using System;
+using Godot;
+
+public partial class MouseDrag : RigidBody2D
+{
+	public float mouseFollowSpeed = 30f;
+	public float objectRotationSpeed = 10f;
+
+	private bool isHeld = false;
+
+	private Vector2 mouseClickOffset;
+
+	public Action<MouseDrag> OnBeginDragging;
+
+	public override void _Ready()
+	{
+		//These 2 are needed for the object to interract correctly with physics when picked up
+		FreezeMode = FreezeModeEnum.Kinematic;
+		InputPickable = true;
+
+        //calls the OnInputEvent method when the collider detects user input overlapping it
+		InputEvent += OnInputEvent;
+	}
+
+
+	public void OnInputEvent(Node viewport, InputEvent @event, long shape_idx)
+	{	
+		var isMouseEvent = @event is InputEventMouseButton;
+		
+		if (!isMouseEvent) return;
+		
+		var mouseEvent = (InputEventMouseButton)@event;
+
+		var pressed = mouseEvent.Pressed;
+		var isLeftClick = mouseEvent.GetButtonIndex() == MouseButton.Left;
+		
+		if (isLeftClick && pressed) 
+			BeginDragging();
+	}
+
+	private void BeginDragging()
+	{
+		mouseClickOffset = GlobalPosition - GetGlobalMousePosition();
+		isHeld = true;
+		Freeze = true;
+
+		OnBeginDragging?.Invoke(this);
+	}
+
+	public void EndDragging()
+	{
+		isHeld = false;
+		Freeze = false;
+	}
+}
